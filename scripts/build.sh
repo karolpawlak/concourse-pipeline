04f040065a04d83410c91e1c6bd55b430a5274d3
karolpawlak <parol.kawlak@gmail.com>
2022-05-23 15:29:06 +0100

    Edited sh with Java install

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,15 @@ ls -l
 
 echo
 echo "------------------------------------------------"
+echo "Installing JDK and JRE in order to run Java applications and applets"
+echo "------------------------------------------------"
+echo
+
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
+java --version
+
+echo
+echo "------------------------------------------------"
 echo "Change directory and create build"
 echo "------------------------------------------------"
 echo
